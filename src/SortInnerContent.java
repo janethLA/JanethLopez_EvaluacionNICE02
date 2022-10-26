7fb88377a75afc1aec00e0b24e9eef00c24291bc
janeth <jl715643@gmail.com>
2022-10-26 19:02:00 -0400

    Ejercicio resuelto

--- src/SortInnerContent.java
+++ src/SortInnerContent.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortInnerContent {
 
@@ -15,9 +17,10 @@ public class SortInnerContent {
 		for(int i=0;i<message.length();i++) {
 			w+=message.charAt(i);
 			if((int)message.charAt(i)==32) {
-				words.add(w);
+				words.add(w.trim());
 				w="";
 			}
+			
 			if(i==message.length()-1) {
 				words.add(w);
 			}
@@ -33,7 +36,7 @@ public class SortInnerContent {
 		String result="";
 		result+=word.charAt(0);
 		String contenidoInterno = word.substring(1, word.length()-1);
-		result+=contenidoInterno=ordenarContenidoInterno(contenidoInterno);
+		result+=ordenarContenidoInterno(contenidoInterno);
 		result+=word.charAt(word.length()-1);
 		return result;
 	}
@@ -41,16 +44,30 @@ public class SortInnerContent {
 	private String ordenarContenidoInterno(String m) {
 		String result="";
 		String m1=m;
-		for(int j=0;j<m.length();j++) {
-			if((int)m.charAt(j)>(int)m.charAt(j+1)) {
-				result+= m.charAt(j);
-			}
-		}
+		char c, c1;  //ndu
+		int pos=0;
+		Character[] array =  convertStringToArray(m);
+        Arrays.sort(array, Comparator.reverseOrder());
+		result = convertArrayToString(array);
 		return result;
 	}
 	
+	private Character[] convertStringToArray(String m) {
+		Character[] result=new Character[m.length()];
+		for(int i=0;i<m.length();i++) {
+			result[i]=m.charAt(i);
+		}
+		return result;
+	}
+	private String convertArrayToString(Character[] c) {
+		String result="";
+		for(int i=0;i<c.length;i++) {
+			result+=c[i];
+		}
+		return result;
+	}
 	public static void main (String args []) {
-		SortInnerContent e= new SortInnerContent("Hola mundo");
+		SortInnerContent e= new SortInnerContent("sort the inner content in descending order");
 	    System.out.println(e.orderContentMessage());
 
 	}
